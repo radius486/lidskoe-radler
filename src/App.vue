6b35c2cd968aec41d1555dd4eb19baa0086abb8b
Rodion Grishaev <radius486@gmail.com>
2017-06-29 06:39:24 +0300

    Vue.js application

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <header-component></header-component>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script>
+import headerComponent from './components/header-component.vue';
+import footerComponent from './components/footer-component.vue';
+
+export default {
+  name: 'app',
+  components: {
+    headerComponent,
+    footerComponent
+  }
+}
+</script>
+
+<style>
+  html, body {
+    min-height: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    background-color: #afcb05;
+  }
+</style>
---
+++ src/components/header-component.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <div class="header-inner">
+      <div class="logo">
+        <a href="/" title="Галоўная">Галоўная</a>
+      </div>
+      <div class="header-menu">
+        <a href="#">Галоўная</a>
+        <a href="#">Галасаваць</a>
+        <a href="#">Больш пра ЛIДСКАЕ® Radler</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header-component',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../assets/styles/header-component.css'
+</style>
