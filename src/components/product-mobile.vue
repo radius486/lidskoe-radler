c6aed59f777282d4279c61feb4aff0c5acca02f6
Rodion Grishaev <radius486@gmail.com>
2017-07-03 22:21:59 +0300

    Product slider for mobile.

--- src/components/product-mobile.vue
+++ src/components/product-mobile.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="product">
-    <div class="product_content">
-      <div class="product_info"></div>
+    <a href="#" @click.prevent='prevSlide' class="prev-slide" :class="{green: currentSlideNum == 2}">Prev</a>
+    <a href="#" @click.prevent='nextSlide' class="next-slide" :class="{green: currentSlideNum == 2}">Next</a>
+    <div class="slides" :style='{transform: "translateX(" + offset + ")" }'>
+      <div class="product_content slide1">
+        <div class="product_info"></div>
+      </div>
+      <div class="product_content slide2">
+        <div class="product_info"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,7 +18,28 @@
     name: 'product-mobile',
     data () {
       return {
+        currentSlideNum: 1
+      }
+    },
 
+    computed: {
+      offset() {
+        let height = window.innerWidth;
+        return '-' + height * (this.currentSlideNum - 1)  + 'px'
+      }
+    },
+
+    methods: {
+      nextSlide() {
+        if (this.currentSlideNum == 1) {
+          this.currentSlideNum++;
+        }
+      },
+
+      prevSlide() {
+        if (this.currentSlideNum == 2) {
+          this.currentSlideNum--;
+        }
       }
     }
   }
